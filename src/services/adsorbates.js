6e5434e4b0c58cd72686218b2caa4ec435e8915f
Lucas Lavandeira <llavandeira@devartis.com>
2021-04-21 19:32:03 -0300

    Add trailing slash to route

--- src/services/adsorbates.js
+++ src/services/adsorbates.js
@@ -2,7 +2,7 @@ import {settings} from "../config/settings";
 import {HttpClient} from "./http_client";
 
 export const getAdsorbates = async () => {
-  const endpoint = `${settings.BACKEND_URL}adsorbato`;
+  const endpoint = `${settings.BACKEND_URL}adsorbato/`;
   const client = new HttpClient(null);
   return (await client.get(endpoint)).data;
 };
