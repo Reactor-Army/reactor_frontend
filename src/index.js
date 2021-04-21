ab2b6d57d3412c87ecd48c6310d2c87cdd709912
Lucas Lavandeira <llavandeira@devartis.com>
2021-04-21 19:30:13 -0300

    Executes get adsorbates only while in adsorbate view

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 import store from "./redux/store";
-import {fetchAdsorbates} from "./redux/adsorbatesSlice";
-
-store.dispatch(fetchAdsorbates());
 
 ReactDOM.render(
   <Provider store={store}>
