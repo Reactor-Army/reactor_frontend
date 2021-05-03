c70ad6e1813f085c3ad1a2324c71ef8af8361d5d
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-05-02 23:41:46 -0300

    Added changes to avoid sidebar overlaping with other components

--- src/routing/PrivateRoute.js
+++ src/routing/PrivateRoute.js
@@ -2,6 +2,7 @@ import {Route, Redirect} from "react-router-dom";
 import React from "react";
 import {Sidebar} from "../components/Sidebar/Sidebar";
 import {PageContainer} from "../components/PageContainer/PageContainer";
+import {PageContent} from "../components/PageContent/PageContent";
 
 export const PrivateRoute = ({component: Component, authed, ...rest}) => {
   return authed === 0 ? (
@@ -10,7 +11,9 @@ export const PrivateRoute = ({component: Component, authed, ...rest}) => {
       render={(props) => (
         <PageContainer>
           <Sidebar />
-          <Component {...props} />
+          <PageContent>
+            <Component {...props} />
+          </PageContent>
         </PageContainer>
       )}
     />
