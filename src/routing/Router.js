e52ca922fc4740eda8b130570e3a795f21ffbabb
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-05-02 21:30:24 -0300

    Removed duplicated sidebar

--- src/routing/Router.js
+++ src/routing/Router.js
@@ -3,31 +3,26 @@ import React from "react";
 import {HomeRoute} from "./routes/HomeRoute";
 import {AdsorbatesRoute} from "./routes/AdsorbatesRoute";
 import {AdsorbentsRoute} from "./routes/AdsorbentsRoute";
-import {Sidebar} from "../components/Sidebar/Sidebar";
-import {PageContainer} from "../components/PageContainer/PageContainer";
 import {PrivateRoute} from "./PrivateRoute";
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <PageContainer>
-        <Sidebar />
-        <Switch>
-          <PrivateRoute
-            authed={0}
-            exact
-            path={"/adsorbatos"}
-            component={AdsorbatesRoute}
-          />
-          <PrivateRoute
-            authed={0}
-            exact
-            path={"/adsorbentes"}
-            component={AdsorbentsRoute}
-          />
-          <PrivateRoute authed={0} exact path={"/"} component={HomeRoute} />
-        </Switch>
-      </PageContainer>
+      <Switch>
+        <PrivateRoute
+          authed={0}
+          exact
+          path={"/adsorbatos"}
+          component={AdsorbatesRoute}
+        />
+        <PrivateRoute
+          authed={0}
+          exact
+          path={"/adsorbentes"}
+          component={AdsorbentsRoute}
+        />
+        <PrivateRoute authed={0} exact path={"/"} component={HomeRoute} />
+      </Switch>
     </BrowserRouter>
   );
 };
