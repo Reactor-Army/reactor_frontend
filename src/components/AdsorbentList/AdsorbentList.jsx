59182c0eaa764e5264702736c994eea94e5569ed
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-05-06 20:21:42 -0300

    Added card layout for adsorbents

--- src/components/AdsorbentList/AdsorbentList.jsx
+++ src/components/AdsorbentList/AdsorbentList.jsx
@@ -4,26 +4,32 @@ import {fetchAdsorbents} from "../../redux/adsorbentsSlice";
 
 import {AdsorbentCard} from "../../components/AdsorbentCard/AdsorbentCard";
 import {appColors} from "../../common/styles";
+import {ListContainer} from "./Styles";
 
-export function AdsorbentList({loading, adsorbates}) {
+export function AdsorbentList({loading, adsorbents}) {
   //const {adsorbents} = useSelector((state) => state.adsorbents);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAdsorbents());
   }, []);
-
   console.log(loading);
-  console.log(adsorbates);
   return (
-    <>
-      <AdsorbentCard
-        backgroundColor={appColors.adsorbentCard}
-        header={"Dolomita"}
-        particleSize={"53-74 µm"}
-        sBet={"1,83"}
-        vBet={"0,0003"}
-        ph={"4,43"}
-      />
-    </>
+    <ListContainer>
+      {adsorbents.length &&
+        adsorbents.map(
+          ({nombre, particulaT, sBet, vBet, pHCargaCero}, index) => (
+            <AdsorbentCard
+              backgroundColor={appColors.adsorbentCard}
+              header={nombre}
+              particleSize={particulaT}
+              sBet={sBet}
+              vBet={vBet}
+              ph={pHCargaCero}
+              key={index}
+            />
+          ),
+        )}
+      ;
+    </ListContainer>
   );
 }
