d4957dce80ee5d315e8b5f099843bd7e2bb019d5
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-07 17:39:56 -0300

    Remove unused imports

--- src/components/Search/AdsorbentSearch.js
+++ src/components/Search/AdsorbentSearch.js
@@ -1,14 +1,8 @@
 import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
 import {Search} from "./Search";
 import {Name} from "./Fields/Name";
 
-export function AdsorbentSearch({
-  name,
-  handleNameChange,
-  onSearchSubmit,
-}) {
+export function AdsorbentSearch({name, handleNameChange, onSearchSubmit}) {
   return (
     <Search onSearchSubmit={onSearchSubmit}>
       <Name name={name} handleNameChange={handleNameChange} />
