011aef7daab84ed92cdd4b55873e0b2355d8e065
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-07 17:37:59 -0300

    Extract IonicCharge to a dedicated component

--- src/components/Search/AdsorbateSearch.jsx
+++ src/components/Search/AdsorbateSearch.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
 import {Search} from "./Search";
 import {Name} from "./Fields/Name";
+import {IonicCharge} from "./Fields/IonicCharge";
 
 export function AdsorbateSearch({
   name,
@@ -14,16 +13,7 @@ export function AdsorbateSearch({
   return (
     <Search onSearchSubmit={onSearchSubmit}>
       <Name name={name} handleNameChange={handleNameChange} />
-      <Grid item xs={3}>
-        <TextField
-          id="outlined-basic"
-          label="Carga iónica"
-          type="number"
-          variant="outlined"
-          value={charge}
-          onChange={handleChargeChange}
-        />
-      </Grid>
+      <IonicCharge charge={charge} handleChargeChange={handleChargeChange} />
     </Search>
   );
 }
