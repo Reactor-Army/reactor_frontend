210e4dcd428121ad74b50718c97166dc12b0002f
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-05-06 22:35:21 -0300

    Removed console.log

--- src/components/AdsorbentList/AdsorbentList.jsx
+++ src/components/AdsorbentList/AdsorbentList.jsx
@@ -6,13 +6,11 @@ import {AdsorbentCard} from "../../components/AdsorbentCard/AdsorbentCard";
 import {appColors} from "../../common/styles";
 import {ListContainer} from "./Styles";
 
-export function AdsorbentList({loading, adsorbents}) {
-  //const {adsorbents} = useSelector((state) => state.adsorbents);
+export function AdsorbentList({adsorbents}) {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAdsorbents());
   }, []);
-  console.log(loading);
   return (
     <ListContainer>
       {adsorbents.length &&
