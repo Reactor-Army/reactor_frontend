835f45a76d38574adcf1f9fcb451d8379a8233a3
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-14 23:42:55 -0300

    Read process detail data from backend

--- src/containers/Detail/ProcessDetailContainer.js
+++ src/containers/Detail/ProcessDetailContainer.js
@@ -1,10 +1,19 @@
 import {Typography} from "@material-ui/core";
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchProcess} from "../../redux/processSlice";
 
 export const ProcessDetailContainer = ({processId}) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProcess(processId));
+  }, []);
+
+  const process = useSelector((store) => store.process.process);
   return (
     <div>
       <Typography>Detalle de proceso ID {processId}!</Typography>
+      {process ? <Typography>{process.id}</Typography> : null}
     </div>
   );
 };
---
+++ src/redux/processSlice.js
@@ -0,0 +1,30 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {getProcess} from "../services/processes";
+
+export const fetchProcess = createAsyncThunk("fetchProcess", getProcess);
+
+const processesSlice = createSlice({
+  name: "process",
+  initialState: {
+    process: null,
+    loading: false,
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchProcess.pending]: (state) => {
+      state.loading = true;
+      state.process = null;
+    },
+    [fetchProcess.fulfilled]: (state, action) => {
+      state.loading = false;
+      console.log(action);
+      state.process = action.payload;
+    },
+    [fetchProcess.rejected]: (state) => {
+      state.loading = false;
+      state.process = null;
+    },
+  },
+});
+
+export default processesSlice.reducer;
