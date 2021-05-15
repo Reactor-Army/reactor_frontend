a8d1291df9c9321737aa9cb11b91c0675a82dea6
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-15 09:40:54 -0300

    Extract Label container to styled component

--- src/components/Detail/Label.jsx
+++ src/components/Detail/Label.jsx
@@ -1,11 +1,12 @@
 import {Typography} from "@material-ui/core";
 import React from "react";
+import {LabelContainer} from "./Styles";
 
 export const Label = ({label, value}) => {
   return (
-    <div style={{display: "flex"}}>
+    <LabelContainer>
       <Typography style={{fontWeight: "bold"}}>{label}</Typography>
       <Typography>: {value}</Typography>
-    </div>
+    </LabelContainer>
   );
 };
---
+++ src/components/Detail/Styles.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components/macro";
+
+export const LabelContainer = styled.div`
+  display: flex;
+`;
