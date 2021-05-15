764e63692257eb9a9fc1238ca429e00763534bd5
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-15 11:38:30 -0300

    Add styles to process detail view

--- src/components/Detail/ProcessDetail.jsx
+++ src/components/Detail/ProcessDetail.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import {PageTitle} from "../../common/PageTitle";
-import {Typography} from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import {Label} from "./Label";
+import {SectionHeader} from "./SectionHeader";
+import {capitalizeFirstLetter} from "../../utils/capitalize";
 
 export const ProcessDetail = ({process}) => {
   return (
     <Container>
-      <PageTitle title={`Detalle del proceso}`} />
-      <Typography>Adsorbato: {process.adsorbato.nombreIon}</Typography>
-      <Typography>Adsorbente: {process.adsorbente.nombre}</Typography>
-      <Label label={"qMax"} value={process.qmax} />
-      <Typography>
-        Tiempo de equilibrio: {process.tiempoEquilibrio} minutos
-      </Typography>
-      <Typography>pH inicial: {process.phinicial}</Typography>
-      <Typography>
-        Proceso de complejación: {booleanToString(process.complejacion)}
-      </Typography>
-      <Typography>
-        Proceso con intercambio iónico:{" "}
-        {booleanToString(process.intercambioIonico)}
-      </Typography>
-      <Typography>
-        Reacción química: {booleanToString(process.reaccionQuimica)}
-      </Typography>
-      <Typography>Fuente: {process.fuente}</Typography>
-      <Typography>Observaciones: {process.observacion}</Typography>
+      <PageTitle title={`Proceso`} />
+      <Label
+        label={"Adsorbato"}
+        value={capitalizeFirstLetter(process.adsorbato.nombreIon)}
+      />
+      <Label label={"Adsorbente"} value={process.adsorbente.nombre} />
+      <SectionHeader>Propiedades</SectionHeader>
+      <Label label={"qMax"} value={`${process.qmax} g/mmol`} />
+      <Label
+        label={"Tiempo de equilibrio"}
+        value={`${process.tiempoEquilibrio} minutos`}
+      />
+      <Label label={"pH inicial"} value={process.phinicial} />
+      <SectionHeader>Detalles del proceso</SectionHeader>
+      <Label
+        label={"Es un proceso de complejación"}
+        value={booleanToString(process.complejacion)}
+      />
+      <Label
+        label={"Es un proceso con intercambio iónico"}
+        value={booleanToString(process.intercambioIonico)}
+      />
+      <Label
+        label={"Es una reacción química"}
+        value={booleanToString(process.reaccionQuimica)}
+      />
+
+      <SectionHeader>Adicional</SectionHeader>
+      <Label label={"Fuente"} value={process.fuente} />
+      <Label label={"Observaciones"} value={process.observacion || "-"} />
     </Container>
   );
 };
--- src/components/Detail/Styles.jsx
+++ src/components/Detail/Styles.jsx
@@ -3,3 +3,7 @@ import styled from "styled-components/macro";
 export const LabelContainer = styled.div`
   display: flex;
 `;
+
+export const SectionHeaderContainer = styled.div`
+  margin-top: 20px;
+`;
