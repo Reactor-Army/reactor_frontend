fa6d5f6ec9509be924294d769ebdd85f8f75e94a
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-05-15 23:50:49 -0300

    Commitining missing file

--- src/components/PageContainer/Styles.jsx
+++ src/components/PageContainer/Styles.jsx
@@ -1,11 +1,9 @@
 import styled from "styled-components/macro";
-import {appColors} from "../../common/styles";
 
 export const Container = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
   min-height: 100%;
-  background-color: ${appColors.pageBackground};
   background-size: cover;
 `;
