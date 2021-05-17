04726fd5782c4763daeed773cf3b3383c8e210c1
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-17 20:22:16 -0300

    Remove more custom styles

--- src/routing/PrivateRoute.js
+++ src/routing/PrivateRoute.js
@@ -1,18 +1,15 @@
 import {Route, Redirect} from "react-router-dom";
 import React from "react";
-import {PageContainer} from "../components/PageContainer/PageContainer";
-import {PageContent} from "../components/PageContent/PageContent";
+import Container from "@material-ui/core/Container";
 
 export const PrivateRoute = ({component: Component, authed, ...rest}) => {
   return authed === 0 ? (
     <Route
       {...rest}
       render={(props) => (
-        <PageContainer>
-          <PageContent>
-            <Component {...props} />
-          </PageContent>
-        </PageContainer>
+        <Container>
+          <Component {...props} />
+        </Container>
       )}
     />
   ) : (
