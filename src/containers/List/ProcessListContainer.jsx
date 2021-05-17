70c7cbc1ecfd16a8cf1d7ebcf97ae4cce0bf3142
Lucas Lavandeira <llavandeira@devartis.com>
2021-05-17 20:36:37 -0300

    Remove extra containers

--- src/containers/List/ProcessListContainer.jsx
+++ src/containers/List/ProcessListContainer.jsx
@@ -8,7 +8,6 @@ import {ProcessList} from "../../components/ProcessList/ProcessList";
 import {ProcessSearchContainer} from "./Search/ProcessSearchContainer";
 import {appFontFamily} from "../../common/styles";
 import {PageTitle} from "../../common/PageTitle";
-import Container from "@material-ui/core/Container";
 
 export function ProcessListContainer() {
   const loading = useSelector((state) => state.loading);
@@ -29,13 +28,13 @@ export function ProcessListContainer() {
   }, []);
 
   return (
-    <Container>
+    <>
       <PageTitle title={"Procesos"} style={{fontFamily: appFontFamily.card}} />
       <ProcessSearchContainer
         selectedAdsorbateId={query.get("adsorbato")}
         selectedAdsorbentId={query.get("adsorbente")}
       />
       <ProcessList loading={loading} processes={processes} />
-    </Container>
+    </>
   );
 }
