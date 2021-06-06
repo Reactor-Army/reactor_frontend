7167dc8edcedd10b42cd37e6f1fe3c04564b87ea
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-06-06 19:10:35 -0300

    Changed prop name and added whitespace for a prop

--- src/components/Card/AdsorbentCard/Ph.jsx
+++ src/components/Card/AdsorbentCard/Ph.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import {BodyText, FieldLabel, FieldValue} from "../CommonCardStyles";
 
 export const Ph = ({ph}) => {
+  console.log("PH:", ph);
   return (
     <BodyText>
-      <FieldLabel>pH(carga cero):</FieldLabel>
+      <FieldLabel>pH (carga cero):</FieldLabel>
       <FieldValue>{ph}</FieldValue>
     </BodyText>
   );
