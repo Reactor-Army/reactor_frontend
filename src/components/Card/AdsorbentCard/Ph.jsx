b77676c7691a6e79b4504ed245efc0cc8fbc8a71
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-06-07 10:20:18 -0300

    Removed console log

--- src/components/Card/AdsorbentCard/Ph.jsx
+++ src/components/Card/AdsorbentCard/Ph.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {BodyText, FieldLabel, FieldValue} from "../CommonCardStyles";
 
 export const Ph = ({ph}) => {
-  console.log("PH:", ph);
   return (
     <BodyText>
       <FieldLabel>pH (carga cero):</FieldLabel>
