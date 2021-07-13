8f6956c10f8fdd216b29f1fe4221e9c84e27de86
matiasReimondo <matiasreimondo@gmail.com>
2021-07-12 21:05:39 -0300

    Migracion a formik

---
+++ src/components/Create/AdsorbateForm.jsx
@@ -0,0 +1,103 @@
+import React, {useState, useEffect} from "react";
+import {Form} from "../Form/Form";
+import {FormTextField, FormNumericField} from "../Form/Fields/FormFields";
+import {ADSORBATE_FIELDS} from "../../common/fields";
+import {isSet} from "../Form/Validation/formValidations";
+import {ADSORBATE_FORM_INITIAL_VALUES} from "../../common/constants";
+
+export const AdsorbateForm = ({
+  title,
+  onSubmit,
+  buttonLabel,
+  setErrors,
+  initialValues,
+}) => {
+  const [initial, setInitial] = useState(ADSORBATE_FORM_INITIAL_VALUES);
+
+  const [errorValues, setErrorValues] = useState({
+    nombreIon: null,
+    nombreIUPAC: null,
+    cargaIon: null,
+  });
+
+  useEffect(() => {
+    if (initialValues) {
+      setInitial({
+        nombreIon: initialValues.nombreIon,
+        nombreIUPAC: initialValues.nombreIUPAC,
+        cargaIon: initialValues.cargaIon,
+        radioIonico: initialValues.radioIonico,
+        limiteVertido: initialValues.limiteVertido,
+        formula: initialValues.formula,
+      });
+    }
+  }, [initialValues]);
+
+  const errorsSet = Object.keys(errorValues).some((key) => {
+    return errorValues[key] !== undefined;
+  });
+
+  useEffect(() => {
+    setErrors(errorsSet);
+  }, [errorsSet]);
+
+  return (
+    <Form
+      initialValues={initial}
+      onSubmit={onSubmit}
+      title={title}
+      buttonLabel={buttonLabel}
+      errors={errorsSet}
+      fields={[
+        <FormTextField
+          placeholder={ADSORBATE_FIELDS.ION_NAME}
+          key={1}
+          name="nombreIon"
+          error={errorValues["nombreIon"]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {...previousState, nombreIon: isSet(value)};
+            });
+          }}
+        />,
+        <FormTextField
+          placeholder={ADSORBATE_FIELDS.IUPAC_NAME}
+          key={2}
+          name="nombreIUPAC"
+          error={errorValues["nombreIUPAC"]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {...previousState, nombreIUPAC: isSet(value)};
+            });
+          }}
+        />,
+        <FormNumericField
+          placeholder={ADSORBATE_FIELDS.ION_CHARGE}
+          key={3}
+          name="cargaIon"
+          error={errorValues["cargaIon"]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {...previousState, cargaIon: isSet(value)};
+            });
+          }}
+        />,
+        <FormNumericField
+          placeholder={ADSORBATE_FIELDS.ION_RADIUS}
+          key={4}
+          name="radioIonico"
+        />,
+        <FormNumericField
+          placeholder={ADSORBATE_FIELDS.SPILL_LIMIT}
+          key={5}
+          name="limiteVertido"
+        />,
+        <FormTextField
+          placeholder={ADSORBATE_FIELDS.FORMULA}
+          key={2}
+          name="formula"
+        />,
+      ]}
+    />
+  );
+};
