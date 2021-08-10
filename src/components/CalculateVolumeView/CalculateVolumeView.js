0e48f50040169bf0be6af2a1c8055aa73c4dad33
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-08-10 19:37:55 -0300

    Added logic to make a form invalid, in case a user wants to use a link to calculate the volume of a system with null fields

--- src/components/CalculateVolumeView/CalculateVolumeView.js
+++ src/components/CalculateVolumeView/CalculateVolumeView.js
@@ -5,10 +5,11 @@ import {CalculateVolumeForm} from "./CalculateVolumeForm";
 import {Label} from "../Detail/Label";
 import {SectionHeader} from "../Detail/SectionHeader";
 import {ProcessCardWrapper} from "../Card/ProcessCard/ProcessCardWrapper";
-import {CardContainer} from "./Styles";
+import {CardContainer, InvalidFormMessage} from "./Styles";
 import {getKineticConstantUnits} from "../../common/UnitsUtils";
 
 export const CalculateVolumeView = ({process, onSubmit}) => {
+  const validForm = !process.ordenReaccion || !process.constanteCinetica;
   return (
     <>
       <PageTitle title={"Calcular Volumen Efectivo"} />
@@ -36,7 +37,13 @@ export const CalculateVolumeView = ({process, onSubmit}) => {
         label={"Orden de la reacción (n)"}
         value={process.ordenReaccion ? process.ordenReaccion : "-"}
       />
-      <CalculateVolumeForm onSubmit={onSubmit} />
+      {validForm && (
+        <InvalidFormMessage>
+          *No se puede calcular el volumen efectivo para este sistema dado que
+          no tiene asignada una constante cinética o un orden de reacción.
+        </InvalidFormMessage>
+      )}
+      <CalculateVolumeForm onSubmit={onSubmit} forceDisable={validForm} />
     </>
   );
 };
--- src/components/CalculateVolumeView/Styles.js
+++ src/components/CalculateVolumeView/Styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {appFontColors, appFontFamily} from "../../common/styles";
 
 export const CalculateVolumeFormContainer = styled.div`
   margin-top: 20px;
@@ -7,8 +8,15 @@ export const CalculateVolumeFormContainer = styled.div`
 export const CardContainer = styled.div`
   width: 600px;
   margin-top: 15px;
-  margin-bottom: 15px;
+  margin-bottom: 20px;
   @media (max-width: 1110px) {
     width: 100%;
   } ;
+`;
+
+export const InvalidFormMessage = styled.div`
+  margin-top: 10px;
+  font-family: ${appFontFamily.primary};
+  color: ${appFontColors.warning};
+  font-size: 16px;
 `;
