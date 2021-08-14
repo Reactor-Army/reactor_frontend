ec6165c8262c89f3bea91b2728cd23242cd02e2b
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-08-14 17:40:41 -0300

    Changed not found page path

--- src/routing/urls.js
+++ src/routing/urls.js
@@ -14,7 +14,7 @@ export const URLS = {
   PROCESS_EDIT: "/sistemas/:id/modificar",
   PROCESS_EFFECTIVE_VOLUME: "/sistemas/:id/volumen",
   BEST_ADSORBENT: "/mejor-adsorbente",
-  NOT_FOUND: "/NotFound",
+  NOT_FOUND: "/not-found",
 };
 
 const processUrlId = (url, id) => {
