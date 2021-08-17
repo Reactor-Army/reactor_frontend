e75d88038af36fd76095ace2afa5822881104148
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-08-17 20:36:07 -0300

    Removed plot from home page

--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-
 import {HomeTitle} from "./Styles";
 import {HomepageWallText} from "./HomepageWallText";
-import {ThomasModelPlot} from "../ThomasModelPlot/ThomasModelPlot";
 
 export function Homepage() {
   return (
     <>
-      <ThomasModelPlot F={0} C0={42.1} W={4.612} Kth={0.66} q0={2.39} />
       <HomeTitle>Reactor App </HomeTitle>
       <HomepageWallText />
     </>
