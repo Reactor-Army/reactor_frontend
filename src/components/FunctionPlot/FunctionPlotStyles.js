b7258c44dc34d6e7251fbf3ce127927a70cc637d
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-08-21 11:52:18 -0300

    Fixed styles so points look bigger, axis labels fonts increase and the stroke width got enhanced as well

--- src/components/FunctionPlot/FunctionPlotStyles.js
+++ src/components/FunctionPlot/FunctionPlotStyles.js
@@ -8,4 +8,25 @@ export const PlotWrapper = styled.div`
 export const Plot = styled.div`
   width: 100%;
   height: 100%;
+
+  .axis-label {
+    font-weight: bold;
+  }
+
+  circle {
+    r: 4;
+  }
+
+  .line-0 {
+    stroke-width: 3px;
+    stroke-linecap: round;
+  }
+
+  text {
+    font-size: 15px;
+
+    @media (max-width: 400px) {
+      font-size: 12px;
+    }
+  }
 `;
