7eb889b503786dbdd23fd10bc01adbece0e2902e
Lucas Lavandeira <llavandeira@devartis.com>
2021-08-23 20:19:08 -0300

    Add sample FileField

--- src/containers/Models/ThomasContainer.js
+++ src/containers/Models/ThomasContainer.js
@@ -1,6 +1,9 @@
 import {Form} from "../../components/Form/Form";
 import {filterBlank} from "../../components/Create/validations";
-import {FormNumericField} from "../../components/Form/Fields/FormFields";
+import {
+  FileField,
+  FormNumericField,
+} from "../../components/Form/Fields/FormFields";
 import {THOMAS_FIELDS, THOMAS_REQUEST_FIELDS} from "../../common/fields";
 import {
   isPositive,
@@ -8,64 +11,77 @@ import {
 } from "../../components/Form/Validation/formValidations";
 import React, {useState} from "react";
 import {THOMAS_FORM_INITIAL_VALUES} from "../../common/constants";
+import {PageTitle} from "../../common/PageTitle";
 
 export const ThomasContainer = () => {
-  const onSubmit = () => {};
+  const [file, setFile] = useState(null);
+  const onSubmit = (values) => {
+    console.log(values);
+    console.log(file);
+  };
   const buttonLabel = "Calcular";
   const [errorValues, setErrorValues] = useState({});
+  const onFileChange = (event) => {
+    setFile(event.currentTarget.files[0]);
+  };
+
   return (
-    <Form
-      initialValues={THOMAS_FORM_INITIAL_VALUES}
-      onSubmit={onSubmit}
-      title={"Thomas"}
-      buttonLabel={buttonLabel}
-      errors={filterBlank(errorValues)}
-      fields={[
-        <FormNumericField
-          placeholder={THOMAS_FIELDS.FLOW}
-          key={1}
-          name={THOMAS_REQUEST_FIELDS.FLOW}
-          error={errorValues[THOMAS_REQUEST_FIELDS.FLOW]}
-          validate={(value) => {
-            setErrorValues((previousState) => {
-              return {
-                ...previousState,
-                [THOMAS_REQUEST_FIELDS.FLOW]: isSet(value) || isPositive(value),
-              };
-            });
-          }}
-        />,
-        <FormNumericField
-          placeholder={THOMAS_FIELDS.INITIAL_CONCENTRATION}
-          key={1}
-          name={THOMAS_REQUEST_FIELDS.INITIAL_CONCENTRATION}
-          error={errorValues[THOMAS_REQUEST_FIELDS.INITIAL_CONCENTRATION]}
-          validate={(value) => {
-            setErrorValues((previousState) => {
-              return {
-                ...previousState,
-                [THOMAS_REQUEST_FIELDS.INITIAL_CONCENTRATION]:
-                  isSet(value) || isPositive(value),
-              };
-            });
-          }}
-        />,
-        <FormNumericField
-          placeholder={THOMAS_FIELDS.ADSORBENT_MASS}
-          key={1}
-          name={THOMAS_REQUEST_FIELDS.ADSORBENT_MASS}
-          error={errorValues[THOMAS_REQUEST_FIELDS.ADSORBENT_MASS]}
-          validate={(value) => {
-            setErrorValues((previousState) => {
-              return {
-                ...previousState,
-                [THOMAS_REQUEST_FIELDS.ADSORBENT_MASS]:
-                  isSet(value) || isPositive(value),
-              };
-            });
-          }}
-        />,
-      ]}
-    />
+    <>
+      <PageTitle title={"Thomas"} />
+      <FileField onChange={onFileChange} />
+      <Form
+        initialValues={THOMAS_FORM_INITIAL_VALUES}
+        onSubmit={onSubmit}
+        buttonLabel={buttonLabel}
+        errors={filterBlank(errorValues)}
+        fields={[
+          <FormNumericField
+            placeholder={THOMAS_FIELDS.FLOW}
+            key={1}
+            name={THOMAS_REQUEST_FIELDS.FLOW}
+            error={errorValues[THOMAS_REQUEST_FIELDS.FLOW]}
+            validate={(value) => {
+              setErrorValues((previousState) => {
+                return {
+                  ...previousState,
+                  [THOMAS_REQUEST_FIELDS.FLOW]:
+                    isSet(value) || isPositive(value),
+                };
+              });
+            }}
+          />,
+          <FormNumericField
+            placeholder={THOMAS_FIELDS.INITIAL_CONCENTRATION}
+            key={2}
+            name={THOMAS_REQUEST_FIELDS.INITIAL_CONCENTRATION}
+            error={errorValues[THOMAS_REQUEST_FIELDS.INITIAL_CONCENTRATION]}
+            validate={(value) => {
+              setErrorValues((previousState) => {
+                return {
+                  ...previousState,
+                  [THOMAS_REQUEST_FIELDS.INITIAL_CONCENTRATION]:
+                    isSet(value) || isPositive(value),
+                };
+              });
+            }}
+          />,
+          <FormNumericField
+            placeholder={THOMAS_FIELDS.ADSORBENT_MASS}
+            key={3}
+            name={THOMAS_REQUEST_FIELDS.ADSORBENT_MASS}
+            error={errorValues[THOMAS_REQUEST_FIELDS.ADSORBENT_MASS]}
+            validate={(value) => {
+              setErrorValues((previousState) => {
+                return {
+                  ...previousState,
+                  [THOMAS_REQUEST_FIELDS.ADSORBENT_MASS]:
+                    isSet(value) || isPositive(value),
+                };
+              });
+            }}
+          />,
+        ]}
+      />
+    </>
   );
 };
