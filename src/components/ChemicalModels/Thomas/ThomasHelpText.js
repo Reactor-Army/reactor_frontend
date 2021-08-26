438ba8b8c6c94471a342921f34faf6311f7bfec7
Lucas Lavandeira <llavandeira@devartis.com>
2021-08-25 21:20:49 -0300

    small thomas model helptext adjustment

--- src/components/ChemicalModels/Thomas/ThomasHelpText.js
+++ src/components/ChemicalModels/Thomas/ThomasHelpText.js
@@ -4,11 +4,11 @@ import React from "react";
 export const ThomasHelpText = () => {
   return (
     <HelpText>
-      Calcula la constante de Thomas y la capacidad de adsorción en base a un
-      archivo de observaciones. Las observaciones deben ser subidas como un
-      archivo CSV (exportable desde Excel u otro software similar de planillas
-      de cálculo), con dos columnas: &quot;volumenEfluente&quot; medido en
-      mililitros y &quot;C/C0&quot;.
+      Calcula la constante de Thomas (Kth) y la concentración máxima del soluto
+      (q0) en base a un archivo de observaciones. Las observaciones deben ser
+      subidas como un archivo CSV (exportable desde Excel u otro software
+      similar de planillas de cálculo), con dos columnas:
+      &quot;volumenEfluente&quot; medido en mililitros y &quot;C/C0&quot;.
     </HelpText>
   );
 };
