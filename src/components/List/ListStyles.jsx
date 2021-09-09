48e20e0921d546ccc3503bf09bdbcce13538356e
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-09-09 18:32:41 -0300

    Increased grid row width to force early wrapping

--- src/components/List/ListStyles.jsx
+++ src/components/List/ListStyles.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components/macro";
 export const ListContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
   grid-column-gap: 10px;
   grid-row-gap: 20px;
 
-  @media (max-width: 400px) {
+  @media (max-width: 415px) {
     grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
   }
 `;
