9be9b600ae41fc1c582cf03f20b8776471bde4b8
Lucas Lavandeira <llavandeira@devartis.com>
2021-09-18 13:04:47 -0300

    check processes length by greater than

--- src/components/CalculateVolumeView/ProcessPickerResults.js
+++ src/components/CalculateVolumeView/ProcessPickerResults.js
@@ -8,10 +8,10 @@ export const ProcessPickerResults = ({processes, setProcess}) => {
     <>
       <SectionHeader>Resultados de la búsqueda</SectionHeader>
       <ProcessPickerContainer>
-        {processes && processes.length !== 0 && (
+        {processes && processes.length > 0 && (
           <InnerProcessList processes={processes} onProcessClick={setProcess} />
         )}
-        {processes && processes.length === 0 && (
+        {processes && processes.length > 0 && (
           <InvalidFormMessage>
             *No se encontraron sistemas para este adsorbato y adsorbente. Probá
             buscar otra combinación.
