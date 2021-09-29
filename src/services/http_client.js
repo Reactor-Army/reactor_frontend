2f558415e798ad821a3fd7de836ae183ec049448
Lucas Lavandeira <llavandeira@devartis.com>
2021-09-28 22:15:43 -0300

    simplify query construction

--- src/services/http_client.js
+++ src/services/http_client.js
@@ -7,54 +7,19 @@ export class HttpClient {
   }
 
   async get(url, params) {
-    const response = await this.request(
-      url,
-      "GET",
-      undefined,
-      undefined,
-      params,
-    ).catch((error) => {
-      return {
-        data: {
-          status: error.response.status,
-          response: error.response.data,
-        },
-      };
-    });
-    return response;
+    return await this.request(url, "GET", undefined, undefined, params);
   }
 
   async post(url, body) {
-    return await this.request(url, "POST", body).catch((error) => {
-      return {
-        data: {
-          status: error.response.status,
-          response: error.response.data,
-        },
-      };
-    });
+    return await this.request(url, "POST", body);
   }
 
   async delete(url, params) {
-    return await this.request(url, "DELETE", params).catch((error) => {
-      return {
-        data: {
-          status: error.response.status,
-          response: error.response.data,
-        },
-      };
-    });
+    return await this.request(url, "DELETE", params);
   }
 
   async put(url, body) {
-    return await this.request(url, "PUT", body).catch((error) => {
-      return {
-        data: {
-          status: error.response.status,
-          response: error.response.data,
-        },
-      };
-    });
+    return await this.request(url, "PUT", body);
   }
 
   async multiPartPost(url, file, body) {
@@ -80,6 +45,16 @@ export class HttpClient {
       Authorization: `Bearer ${this.token}`,
       "Access-Control-Allow-Origin": "*",
     };
-    return await axios.request({url, method, data, headers, params});
+    return await axios
+      .request({url, method, data, headers, params})
+      .then((response) => response.data)
+      .catch((error) => {
+        return {
+          data: {
+            status: error.response.status,
+            response: error.response.data,
+          },
+        };
+      });
   }
 }
