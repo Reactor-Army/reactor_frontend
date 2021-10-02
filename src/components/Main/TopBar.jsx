b8460d1437a4b7e0ccec5634ab74ca0c9253122e
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-10-02 13:27:16 -0300

    Removed session status from the topbar

--- src/components/Main/TopBar.jsx
+++ src/components/Main/TopBar.jsx
@@ -4,16 +4,6 @@ import {Toolbar} from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import {makeStyles} from "@material-ui/core/styles";
-import {useSelector, useDispatch} from "react-redux";
-import {
-  SessionIndicatorContainer,
-  SessionIndicatorLabel,
-  SessionIndicator,
-  SessionAction,
-} from "./styles";
-import {logout} from "../../redux/auth";
-import {useHistory} from "react-router-dom";
-import {URLS} from "../../routing/urls";
 
 const drawerWidth = 240;
 
@@ -48,9 +38,6 @@ const useStyles = makeStyles((theme) => ({
 
 export const TopBar = ({handleDrawerToggle}) => {
   const classes = useStyles();
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const {loggedIn, userData} = useSelector((state) => state.auth);
 
   return (
     <AppBar position="fixed" className={classes.appBar}>
@@ -63,31 +50,6 @@ export const TopBar = ({handleDrawerToggle}) => {
           className={classes.menuButton}>
           <MenuIcon />
         </IconButton>
-        <SessionIndicatorContainer>
-          {loggedIn ? (
-            <>
-              <SessionIndicatorLabel>
-                Sesión iniciada como:
-              </SessionIndicatorLabel>
-              <SessionIndicator>{userData.email}</SessionIndicator>
-              <SessionAction
-                onClick={() => {
-                  dispatch(logout());
-                }}>
-                Cerrar sesión
-              </SessionAction>
-            </>
-          ) : (
-            <>
-              <SessionAction
-                onClick={() => {
-                  history.push(URLS.LOGIN);
-                }}>
-                Iniciar sesión
-              </SessionAction>
-            </>
-          )}
-        </SessionIndicatorContainer>
       </Toolbar>
     </AppBar>
   );
--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -1,6 +1,5 @@
 import {makeStyles} from "@material-ui/core/styles";
 import styled from "styled-components/macro";
-import {appFontFamily} from "../../common/styles";
 export const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
@@ -15,48 +14,4 @@ export const useStyles = makeStyles((theme) => ({
 
 export const Root = styled.div`
   display: flex;
-`;
-
-export const SessionIndicatorContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-export const SessionIndicatorLabel = styled.div`
-  font-weight: bold;
-  font-family: ${appFontFamily.card};
-  //font-size: calc(12px + 0.4vw);
-  font-size: 16px;
-  margin-right: 5px;
-  white-space: nowrap;
-  @media (max-width: 600px) {
-    display: none;
-    font-size: calc(12px + 0.4vw);
-  }
-`;
-
-export const SessionIndicator = styled.div`
-  font-family: ${appFontFamily.card};
-  //font-size: calc(12px + 0.4vw);
-  font-size: 16px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  max-width: 20ch;
-  @media (max-width: 600px) {
-    display: none;
-  }
-`;
-
-export const SessionAction = styled.div`
-  font-family: ${appFontFamily.card};
-  //font-size: calc(10px + 0.4vw);
-  font-size: 14px;
-  text-decoration: underline;
-  cursor: pointer;
-  font-weight: 500;
-  margin-left: 20px;
-  white-space: nowrap;
-  @media (max-width: 600px) {
-    font-size: 14px;
-  }
 `;
