36c68c30b0fa8637d4624c9783c0274056db930f
Lucas Lavandeira <llavandeira@devartis.com>
2021-10-03 02:44:19 -0300

    Hide edit/delete buttons if user is not an admin

--- src/components/Detail/HeaderButtons.jsx
+++ src/components/Detail/HeaderButtons.jsx
@@ -1,8 +1,15 @@
 import {EditButton} from "../List/common/EditButton";
 import {DeleteButton} from "../List/common/DeleteButton";
 import React from "react";
+import {useSelector} from "react-redux";
+import {settings} from "../../config/settings";
 
 export const HeaderButtons = ({editUrl, onDeleteClick}) => {
+  const role = useSelector((state) => state.auth.userData.rol.nombre);
+  if (role !== settings.ROLE_ADMIN) {
+    return null;
+  }
+
   return (
     <>
       <EditButton url={editUrl} />
