189754dd55ca50ad3de7ea8f1148883926963e8d
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-10-05 20:37:51 -0300

    Added table pagiination

--- src/components/DataGrid/DataGrid.jsx
+++ src/components/DataGrid/DataGrid.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {withStyles, makeStyles} from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -7,6 +7,8 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableFooter from "@material-ui/core/TableFooter";
 import {appColors, appFontFamily} from "../../common/styles";
 
 const StyledTableCell = withStyles((theme) => ({
@@ -33,11 +35,19 @@ const StyledTableRow = withStyles((theme) => ({
 const useStyles = makeStyles({
   table: {
     minWidth: 700,
+    minHeight: 400,
   },
 });
 
 export const DataGrid = ({headerItems, items}) => {
   const classes = useStyles();
+  const rowsPerPage = 4;
+  const [page, setPage] = useState(0);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="customized table">
@@ -52,18 +62,36 @@ export const DataGrid = ({headerItems, items}) => {
         </TableHead>
 
         <TableBody>
-          {items.map((gridItem, index) => (
-            <StyledTableRow key={index}>
-              {Object.values(gridItem).map((value, index) => {
-                return (
-                  <StyledTableCell align="left" key={index}>
-                    {value || "-"}
-                  </StyledTableCell>
-                );
-              })}
-            </StyledTableRow>
-          ))}
+          {items
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((gridItem, index) => (
+              <StyledTableRow key={index}>
+                {Object.values(gridItem).map((value, index) => {
+                  return (
+                    <StyledTableCell align="left" key={index}>
+                      {value || "-"}
+                    </StyledTableCell>
+                  );
+                })}
+              </StyledTableRow>
+            ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              count={items.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              rowsPerPageOptions={[]}
+              labelDisplayedRows={({page}) => {
+                return `Página ${page + 1} de ${Math.ceil(
+                  items.length / rowsPerPage,
+                )}`;
+              }}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
