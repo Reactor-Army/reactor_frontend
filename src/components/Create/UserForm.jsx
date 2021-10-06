5d6d276889644ef332118d634c5eab1c2c07a372
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-10-06 19:48:44 -0300

    Changed form fields

--- src/components/Create/UserForm.jsx
+++ src/components/Create/UserForm.jsx
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from "react";
 import {Form} from "../Form/Form";
-import {FormTextField} from "../Form/Fields/FormFields";
+import {
+  FormTextField,
+  FormSelectorField,
+  FormBigTextField,
+} from "../Form/Fields/FormFields";
 import {USER_FIELDS} from "../../common/fields";
 import {isSet} from "../Form/Validation/formValidations";
 import {ADSORBATE_FORM_INITIAL_VALUES} from "../../common/constants";
@@ -53,6 +57,71 @@ export const UserForm = ({
               };
             });
           }}
+        />,
+        <FormTextField
+          placeholder={USER_FIELDS.LAST_NAME}
+          key={2}
+          name={USER_REQUEST_FIELDS.LAST_NAME}
+          error={errorValues[USER_REQUEST_FIELDS.LAST_NAME]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {
+                ...previousState,
+                [USER_REQUEST_FIELDS.LAST_NAME]: isSet(value),
+              };
+            });
+          }}
+        />,
+        <FormTextField
+          placeholder={USER_FIELDS.EMAIL}
+          key={3}
+          name={USER_REQUEST_FIELDS.EMAIL}
+          error={errorValues[USER_REQUEST_FIELDS.EMAIL]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {
+                ...previousState,
+                [USER_REQUEST_FIELDS.EMAIL]: isSet(value),
+              };
+            });
+          }}
+        />,
+        <FormTextField
+          placeholder={USER_FIELDS.PASSWORD}
+          key={4}
+          name={USER_REQUEST_FIELDS.PASSWORD}
+          error={errorValues[USER_REQUEST_FIELDS.PASSWORD]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {
+                ...previousState,
+                [USER_REQUEST_FIELDS.PASSWORD]: isSet(value),
+              };
+            });
+          }}
+        />,
+        <FormSelectorField
+          key={5}
+          placeholder={USER_FIELDS.ROLE}
+          items={[
+            {label: "Administrador", value: "administrador"},
+            {label: "Usuario", value: "usuario"},
+          ]}
+          name={USER_REQUEST_FIELDS.ROLE}
+          error={errorValues[USER_REQUEST_FIELDS.ROLE]}
+          validate={(value) => {
+            setErrorValues((previousState) => {
+              return {
+                ...previousState,
+                [USER_REQUEST_FIELDS.ROLE]: isSet(value),
+              };
+            });
+          }}
+        />,
+        <FormBigTextField
+          placeholder={USER_FIELDS.DESCRIPTION}
+          key={6}
+          name={USER_REQUEST_FIELDS.DESCRIPTION}
         />,
       ]}
     />
