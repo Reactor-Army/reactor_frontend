d78a2a3f9cbe82def8a794b852c3c38fff601860
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-10-06 21:54:24 -0300

    Reverted config change

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,5 +3,5 @@ export const settings = {
     process.env.REACT_APP_BACKEND_URL || "https://reactor-a1.herokuapp.com/",
   MAX_MODELS: 3,
   MODEL_EQUATION_PRECISION_DIGITS: 3,
-  ADMIN_ROLE: "Administrador",
+  ADMIN_ROLE: "ROLE_ADMIN",
 };
