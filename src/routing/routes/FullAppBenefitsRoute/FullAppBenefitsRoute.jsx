8ee3e2f27c593229b2c3499d34e0dd00e9275dec
Santiago Pinto <santiago.pinto@digitalbeings.net>
2021-10-23 11:47:40 -0300

    Changed full app benefits page text

--- src/routing/routes/FullAppBenefitsRoute/FullAppBenefitsRoute.jsx
+++ src/routing/routes/FullAppBenefitsRoute/FullAppBenefitsRoute.jsx
@@ -15,30 +15,30 @@ export const FullAppBenefitsRoute = () => {
 
         <Paragraph>
           Si no contás con una cuenta para iniciar sesión, quiere decir que
-          estas haciendo uso de una versión limitada de la aplicación. Esto
+          estás haciendo uso de una versión limitada de la aplicación. Esto
           quiere decir que los datos que se están mostrando son solo una parte
           de un conjunto más grande de información que maneja el laboratorio.
           Como usuario invitado no es posible acceder a las funcionalidades de
-          calculo del volumen efectivo de un reactor ni al uso de los distintos
+          cálculo del volumen efectivo de un reactor ni al uso de los distintos
           modelos de curvas de ruptura. En su lugar se muestran únicamente datos
           de ejemplo a modo ilustrativo
         </Paragraph>
         <SmallSubtitle>Beneficios de ser usuario</SmallSubtitle>
         <Paragraph>
           Al ser usuario de la aplicación tendrás acceso a toda la base de datos
-          de adsorbatos, adsorbentes y sistemas que ha estudiado el laboratorio.
-          A su vez, estarán disponibles las funcionalidades de cálculo de
-          volumen efectivo, en caso de que se quiera consultar el tamaño de un
-          reactor a construir, así como al uso de los modelos de curvas de
-          rupturas para evaluar qué tan bueno es un adsorbente para remover
-          ciertos adsorbatos
+          de adsorbatos, adsorbentes y sistemas que han sido estudiados en el
+          LaQuíSiHe. También, estarán disponibles las funcionalidades de cálculo
+          de volumen efectivo, en caso de que quieras consultar el tamaño de un
+          reactor a construir, así como el uso de los modelos de curvas de
+          rupturas para obtener los parámetros característicos de cada modelo
+          para el sistema estudiado y el R2 correspondiente.
         </Paragraph>
         <br />
         <SmallSubtitle>¿Cómo puedo convertirme en usuario?</SmallSubtitle>
         <Paragraph>
           Para obtener las credenciales para ingresar al sistema, contactate con
-          placeholder@mail.com y alguien se pondrá en contacto. No olvides
-          incluir el motivo por el cual requeris las credenciales
+          laquisihe@fi.uba.ar y alguien se pondrá en contacto. No olvides
+          incluir el motivo por el cual requerís las credenciales.
           <br />
           <br />
         </Paragraph>
