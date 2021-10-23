c2aae29f27c8d98b06e1a63f2bbb00207624cf10
Lucas Lavandeira <llavandeira@devartis.com>
2021-10-23 19:51:13 -0300

    Redirect to 404 if model data is not available

--- src/routing/routes/ModelsRoute/ModelResultsRoute.jsx
+++ src/routing/routes/ModelsRoute/ModelResultsRoute.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import {useSelector} from "react-redux";
 import {PageLayout} from "../../../components/ChemicalModels/Models/ModelsStyles";
 import {ModelResults} from "../../../components/ChemicalModels/Results/ModelResults";
+import {Redirect} from "react-router-dom";
+import {URLS} from "../../urls";
 
 export const ModelResultsRoute = () => {
   const modelData = useSelector((state) => state.modelData.models);
-  if (!modelData) {
-    return null;
+
+  if (!modelData.length) {
+    return <Redirect to={URLS.NOT_FOUND} />;
   }
   return (
     <PageLayout>
