49e5470bde0fb987136ee9b15229af0a9d13382c
Lucas Lavandeira <llavandeira@devartis.com>
2021-11-09 00:43:07 -0300

    Fetch charts from backend

--- src/components/ChartArea/ChartPicker.jsx
+++ src/components/ChartArea/ChartPicker.jsx
@@ -1,15 +1,31 @@
 import {Form, FORM_LAYOUTS} from "../Form/Form";
 import {FormSelectorField} from "../Form/Fields/FormFields";
 import {CHART_AREA_FIELDS} from "../../common/fields";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {isSet} from "../Form/Validation/formValidations";
 import {CHART_AREA_FORM_INITIAL_VALUES} from "../../common/constants";
 import {ChartPickerContainer, TitleContainer} from "./Styles";
 import {Title} from "../ChemicalModels/Models/ModelsStyles";
+import {getCurves} from "../../services/processes";
 
-export const ChartPicker = ({onSubmit}) => {
+export const ChartPicker = ({onSubmit, processId}) => {
   const [errorValues, setErrorValues] = useState({});
 
+  const [charts, setCharts] = useState(null);
+  useEffect(async () => {
+    const response = await getCurves(processId);
+
+    setCharts(
+      response
+        .filter((chart) => !chart.lineaBase)
+        .map((chart) => {
+          return {
+            label: chart.nombre,
+            value: chart.id,
+          };
+        }),
+    );
+  });
   const chartIds = [
     {
       label: "Gráfico 1",
@@ -34,7 +50,7 @@ export const ChartPicker = ({onSubmit}) => {
         fields={[
           <FormSelectorField
             key={0}
-            items={chartIds}
+            items={charts}
             placeholder={"Gráfico"}
             name={CHART_AREA_FIELDS.CHART_ID}
             error={errorValues[CHART_AREA_FIELDS.CHART_ID]}
