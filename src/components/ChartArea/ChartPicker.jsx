7efe1ae0098fc945486e28d374f7e7de8dddea2f
Lucas Lavandeira <llavandeira@devartis.com>
2021-11-09 00:48:16 -0300

    Add base lines check

--- src/components/ChartArea/ChartPicker.jsx
+++ src/components/ChartArea/ChartPicker.jsx
@@ -7,11 +7,13 @@ import {CHART_AREA_FORM_INITIAL_VALUES} from "../../common/constants";
 import {ChartPickerContainer, TitleContainer} from "./Styles";
 import {Title} from "../ChemicalModels/Models/ModelsStyles";
 import {getCurves} from "../../services/processes";
+import {MessageContainer} from "../IdealAdsorbentList/Styles";
+import Typography from "@material-ui/core/Typography";
 
 export const ChartPicker = ({onSubmit, processId}) => {
   const [errorValues, setErrorValues] = useState({});
-
   const [charts, setCharts] = useState(null);
+  const [baseLines, setBaseLines] = useState(null);
   useEffect(async () => {
     const response = await getCurves(processId);
 
@@ -25,17 +27,28 @@ export const ChartPicker = ({onSubmit, processId}) => {
           };
         }),
     );
-  });
-  const chartIds = [
-    {
-      label: "Gráfico 1",
-      value: 1,
-    },
-    {
-      label: "Línea base 2",
-      value: 2,
-    },
-  ];
+    setBaseLines(
+      response
+        .filter((chart) => chart.lineaBase)
+        .map((chart) => {
+          return {
+            label: chart.nombre,
+            value: chart.id,
+          };
+        }),
+    );
+  }, [processId]);
+
+  if (baseLines && baseLines.length === 0) {
+    return (
+      <MessageContainer>
+        <Typography>
+          Este sistema no tiene líneas de base cargadas. Seleccioná otro sistema
+          o bien cargá una línea de base para continuar.
+        </Typography>
+      </MessageContainer>
+    );
+  }
   return (
     <ChartPickerContainer>
       <TitleContainer>
@@ -65,7 +78,7 @@ export const ChartPicker = ({onSubmit, processId}) => {
           />,
           <FormSelectorField
             key={1}
-            items={chartIds}
+            items={baseLines}
             placeholder={"Línea base"}
             name={CHART_AREA_FIELDS.BASE_LINE_ID}
             error={errorValues[CHART_AREA_FIELDS.BASE_LINE_ID]}
