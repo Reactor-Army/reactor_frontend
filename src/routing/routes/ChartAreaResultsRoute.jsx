5a15919f9c45e5c97f0c825583440fc09a93140a
Lucas Lavandeira <llavandeira@devartis.com>
2021-11-16 23:57:01 -0300

    Add button to return to area page

--- src/routing/routes/ChartAreaResultsRoute.jsx
+++ src/routing/routes/ChartAreaResultsRoute.jsx
@@ -1,4 +1,4 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import React from "react";
 import {CircularProgress} from "@material-ui/core";
 import {PageTitle} from "../../common/PageTitle";
@@ -7,15 +7,22 @@ import {URLS} from "../urls";
 import {Plot} from "../../components/ChartArea/Plot";
 import {AreaDataFrame} from "../../components/ChartArea/AreaDataFrame";
 import {SystemTitle} from "../../components/ChartArea/SystemTitle";
+import {Button} from "../../components/Button/Button";
+import {setNull} from "../../redux/areaResults";
 
 export const ChartAreaResultsRoute = () => {
   const {loading, results} = useSelector((state) => state.areaResults);
   const history = useHistory();
-
+  const dispatch = useDispatch();
+  const goBack = () => {
+    dispatch(setNull());
+    history.push(URLS.CHART_AREA);
+  };
   if (results === null) {
     // Redirect to chart area root route if user accesses by URL
     history.push(URLS.CHART_AREA);
   }
+
   return (
     <>
       <PageTitle title={"Resultados"} />
@@ -23,6 +30,7 @@ export const ChartAreaResultsRoute = () => {
       <SystemTitle response={results} />
       <AreaDataFrame results={results} />
       <Plot curve={results.curva} baseLine={results.lineaBase} />
+      <Button size="medium" text="Volver" onClick={goBack} />
     </>
   );
 };
